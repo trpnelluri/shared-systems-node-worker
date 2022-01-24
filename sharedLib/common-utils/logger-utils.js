c306dcf3514e9316f1af96b4480080772b3f135e
SivakoteswaraRaoNell <sivakoteswararao.nelluri@cms.hhs.gov>
2022-01-24 10:11:36 -0500

    Updated the code related logger format

--- sharedLib/common-utils/logger-utils.js
+++ sharedLib/common-utils/logger-utils.js
@@ -1,12 +1,15 @@
 'use strict'
 const winston = require('winston');
-const DailyRotateFile = require('winston-daily-rotate-file');
 const moment = require('moment');
 process.setMaxListeners(0);
 
 const timestampMoment = () => moment().format('YYYY/MM/DD HH:mm:ss SSS');
+const workerName = 'shared-systems-node-worker'
+const env = process.env.environment
+const globaltransid = '-'
+const corelationid = '-'
 
-const customLogger = (logFileName, eventName, logParams) =>{
+const customLogger = (eventName, logParams) =>{
     //console.log('in logger utils', logFileName)
     return winston.createLogger({
     // error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5
@@ -15,24 +18,8 @@ const customLogger = (logFileName, eventName, logParams) =>{
             new winston.transports.Console({
                 format: winston.format.combine(
                     winston.format.timestamp({ format: timestampMoment }),
-                    winston.format.printf((info) => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message ? info.message : ''}`),
-                //winston.format.printf((info) => `${info.timestamp},${info.level.toUpperCase()},${logParams.globaltransid || '-' },${eventName},-, ${info.message}`),
+                    winston.format.printf((info) => `${info.timestamp},${info.level.toUpperCase()},${workerName},${env},${logParams.globaltransid || globaltransid },${logParams.corelationid || corelationid },${eventName},${info.message}`),
                 ),
-            }),
-            //new winston.transports.DailyRotateFile({
-            new DailyRotateFile({
-                filename: `${logFileName}%DATE%.log`,
-                json: false,
-                timestamp: timestampMoment,
-                datePattern: 'YYYY-MM-DD',
-                format: winston.format.combine(
-                    winston.format.timestamp({ format: timestampMoment }),
-                    winston.format.printf((info) => `${info.timestamp},${info.level.toUpperCase()},${logParams.globaltransid || '-' },${eventName},-, ${info.message}`),
-                ),
-                handleExceptions: true,
-                humanReadableUnhandledException: true,
-                prettyPrint: true,
-                localTime: true,
             }),
         ],
     })
