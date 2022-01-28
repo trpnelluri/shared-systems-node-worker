a903757532562d5eb50302ac967daf0fa78e0309
SivakoteswaraRaoNell <sivakoteswararao.nelluri@cms.hhs.gov>
2022-01-28 14:27:41 -0500

    Updated the code as per standards

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('dotenv').config({ path: './config/.env' });
 const ParameterStoreData = require('./sharedLib/aws/parameter-store-service');
 ParameterStoreData.loadEnvVariablesFromAWSParamStore();
 const loggerUtils = require('./sharedLib/common/logger-utils');
-const scheduleGenerateFlatfileJob = require('./services/schedule-generate-pa-req-flatfile-job')
+const scheduleGenerateFlatfileJob = require('./services/schedule-job-generate-pa-req-flatfile')
 
 const EventName = 'SS_WORKER_APP';
 let logParams = {};
@@ -18,5 +18,5 @@ app.listen(port, () => {
     logger.info(`app.listen, listining on port: ${port}`);
     // The following function invoke the sqs message consumer service when ever the application starts.
     scheduleGenerateFlatfileJob.schedule_gen_pa_req_flat_file();
-    logger.info('app.listen, scheduleProcessHIHDlq Job Started');
+    logger.info('app.listen, schedule_gen_pa_req_flat_file Job Started');
 });
